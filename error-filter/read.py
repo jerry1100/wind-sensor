3f91ae2240cb44ae9097d1861b788681282a8415
Jerry1100 <jwu1100@gmail.com>
2017-03-13 11:33:07 -1000

    added debugging options

--- error-filter/read.py
+++ error-filter/read.py
@@ -1,4 +1,21 @@
+#!/usr/bin/python
+
+import sys
 import csv
+
+if (len(sys.argv) < 2):
+    print("Use: python read.py <filename>")
+    sys.exit(1)
+
+# Turn debugging on?
+debug = False
+step = False
+filt = False
+if (len(sys.argv) > 2):
+    cmd = sys.argv[2]
+    debug = ('d' in cmd)
+    step = ('s' in cmd)
+    filt = ('f' in cmd)
 
 # Experimental values
 dist = 9.4/100 # in meters
@@ -7,22 +24,26 @@ wind = 0 # m/s
 prop = dist/(331.4 + 0.6*temp + wind)*96*10**6 # clock cycles
 
 # Thresholds based on theoretical prop
-t_lower = prop*0 # play around with these
-t_upper = prop*2
+t_lower = prop*0.5 # play around with these
+t_upper = prop*1.5
 print("Theoretical prop: %d %d" % (prop, prop/96))
 print("Lower threshold: %d %d" % (t_lower, t_lower/96))
 print("Upper threshold: %d %d" % (t_upper, t_upper/96))
 
 # Save data into an array
+num_samples = 0
 data1 = []
 data2 = []
-with open('Data.csv', 'rb') as csvfile:
+with open(sys.argv[1], 'rb') as csvfile:
     data_iterator = csv.reader(csvfile, delimiter=',')
     for t1, t2 in data_iterator:
+        num_samples += 1
         data1.append(int(t1))
         data2.append(int(t2))
 
 # Iterate through data
+if (debug):
+    print("\n***Debugging turned on***")
 times = []
 i1 = i2 = 0
 while (i1 < len(data1) and i2 < len(data2)):
@@ -30,24 +51,28 @@ while (i1 < len(data1) and i2 < len(data2)):
     t2 = data2[i2]/96
 
     diff = data2[i2] - data1[i1]
-    if (diff < t_lower):
-        print("UNDR: %d %d %d KEEP t1" % (t1, t2, diff/96))
+    if (filt and diff < t_lower):
+        if (debug):
+            print("UNDR: %d %d %d KEEP t1" % (t1, t2, diff/96))
         i2 += 1
-    elif (diff > t_upper):
-        print("OVER: %d %d %d KEEP t2" % (t1, t2, diff/96))
+    elif (filt and diff > t_upper):
+        if (debug):
+            print("OVER: %d %d %d KEEP t2" % (t1, t2, diff/96))
         i1 += 1
     else:
-        print("GOOD: %d %d %d STORE" % (t1, t2, diff/96))
+        if (debug):
+            print("GOOD: %d %d %d STORE" % (t1, t2, diff/96))
         times.append(diff)
         i2 += 1
         i1 += 1
-    #raw_input()
+    if (step):
+        raw_input()
 
 # Calculate average
 tot = 0.0
 for t in times:
     tot += t
 average = tot/len(times)
-print("%d sample results: " % len(times))
+print("\n%d sample results (out of %d):" % (len(times), num_samples))
 print("Theoretical prop: %d %dus" % (prop, prop/96))
 print("Experimental prop: %d %dus" % (average, average/96))
