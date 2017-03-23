f742a3d275a4fc636444d3c2a7899e8f7f4497f7
Jerry1100 <jwu1100@gmail.com>
2017-03-23 13:09:53 -1000

    added timing

--- arduino/dev/dev.ino
+++ arduino/dev/dev.ino
@@ -1,3 +1,12 @@
+
+
+// Data structure to store a single sample
+typedef struct Sample {
+    unsigned long emit_times[10];
+    unsigned long recv_times[10];
+    unsigned short recv_val[10];
+} Sample;
+
 void setup() {
 }
 
