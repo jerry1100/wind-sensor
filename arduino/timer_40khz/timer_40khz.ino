d6ba7db53aeb575e15622c5f42fcf24a7fe32251
Jerry1100 <jwu1100@gmail.com>
2017-03-24 20:32:20 -1000

    added timer 40khz sketch

---
+++ arduino/timer_40khz/timer_40khz.ino
@@ -0,0 +1,18 @@
+#include <TimerOne.h>
+
+int pin = 14; // emitter pin
+int state = HIGH; // 0 if low, 1 if high
+
+void setup() {
+  pinMode(pin, OUTPUT);
+  Timer1.initialize(12);
+  Timer1.attachInterrupt(toggle); // toggle pin every 25us
+}
+
+void loop() {
+}
+
+void toggle() {
+  state = (state == HIGH) ? LOW : HIGH;
+  digitalWrite(pin, state);
+}
