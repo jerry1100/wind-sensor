a4eb200293647a4c735fe785e695ab1f70901f6e
Jerry Wu <jwu1100@gmail.com>
2017-04-14 18:14:02 -1000

    sender code

--- arduino/dev/dev.ino
+++ arduino/dev/dev.ino
@@ -10,12 +10,15 @@
 
 volatile byte count;
 volatile byte state;
+unsigned long send_times[2*NUM_PULSES];
 
 void setup() {
+    // Set up clock cycle access
     ARM_DEMCR |= ARM_DEMCR_TRCENA;
     ARM_DWT_CTRL |= ARM_DWT_CTRL_CYCCNTENA;
     pinMode(EMIT_PIN, OUTPUT);
 
+    // Timer1 setup
     Timer1.initialize(12);
     Timer1.attachInterrupt(pulse_isr);
     Timer1.stop();
@@ -30,16 +33,17 @@ void pulse() {
     count = 0;
     state = 0;
 
-    Timer1.restart(); // start the timer, should take 240us
+    Timer1.start(); // start the timer, should take 240us
     delayMicroseconds(240);
 }
 
 void pulse_isr() {
     state = !state;
     digitalWriteFast(EMIT_PIN, state);
+    send_times[count] = CYCLE;
     count++;
 
-    if (count == NUM_PULSES*2) {
+    if (count == 2*NUM_PULSES) {
         Timer1.stop();
     }
 }
